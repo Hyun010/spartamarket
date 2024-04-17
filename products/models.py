4bc2bcb84fa1b7d2f15c911573c153c20974fbf4
Park SungHyun <sunghyun0620@gmail.com>
2024-04-18 00:24:41 +0900

    Models.py 초기 작업 완료 & 회원가입 페이지 이동 완료

--- products/models.py
+++ products/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Product(models.Model):
+    titel=models.CharField(max_length=10)
+    content=models.TextField
+    price=models.DecimalField(max_digits=10,decimal_places=2)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="products")
+
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="like_products")
+
+    def __str__(self):
+        return self.title
--- spartamarket/urls.py
+++ spartamarket/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", views.index, name='index'),
     path("products/",include("products.urls")),
+    path("accounts/",include("accounts.urls")),
 ]
