4588024cad9596a60535a4443ee69915b2c4e1d3
Park SungHyun <sunghyun0620@gmail.com>
2024-04-19 00:26:20 +0900

    기본 완성(CSS 제외)

--- products/models.py
+++ products/models.py
@@ -8,9 +8,7 @@ class Product(models.Model):
     price=models.DecimalField(max_digits=10,decimal_places=0)
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
-
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="products")
-
     jjim_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="jjim_products")
 
     def __str__(self):
