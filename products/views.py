66b7449ab8f2ec272d01312e06fa446541b3d59c
Park SungHyun <sunghyun0620@gmail.com>
2024-04-18 16:35:21 +0900

    게시글 상세정보 보기 완료

--- products/views.py
+++ products/views.py
@@ -12,3 +12,5 @@ def products(request):
 
 def product_detail(request,pk):
     product=get_object_or_404(Product,pk=pk)
+    context={"product": product}
+    return render(request,"products/product_detail.html",context)
